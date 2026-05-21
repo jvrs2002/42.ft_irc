1fe44925f826d92f7aafed69132ad0079639999f
reimanel9999 <158600257+reimanel9999@users.noreply.github.com>
2026-05-21 11:57:20 +0100

    WIP: Join & Part Cmds

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: manelcarvalho <manelcarvalho@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 18:32:03 by joao-vri          #+#    #+#             */
-/*   Updated: 2026/05/18 12:06:44 by manelcarval      ###   ########.fr       */
+/*   Updated: 2026/05/20 18:21:42 by manelcarval      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,36 +22,37 @@
 #include <poll.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <set>
+#include "Client.hpp"
 
 class Channel
 {
 private:
-		std::string					_channel_name;
-		std::vector<std::string>	_users;
-		std::vector<std::string>	_operators;
-		std::string					_topic;
-		
-		// mandatory modes
-		bool _invite;
-		bool _topic;
-		bool _password_active;
-		std::string _password;
-		bool _user_limit_active;
-		int _user_limit;
-		
-				
+	std::string				_channel_name;
+	std::set<Client*>		_users;
+	std::set<Client*>		_operators;
+	
+	// mandatory modes
+	bool _invite_active;
+	std::set<std::string> _invited;
+	bool _topic_active;
+	std::string	_topic;
+	bool _password_active;
+	std::string _password;
+	bool _user_limit_active;
+	int _user_limit;
 		
 public:
-	Channel(/* args */);
+	Channel();
+	Channel(const std::string channel_name, Client* creator);
+	Channel(const Channel& other);
+	Channel& operator=(const Channel &other);
 	~Channel();
+
+	void	joinChannel(Client* new_user, std::string password = "");
+	void	partChannel(Client* user_delete);
+
+	
 };
-
-Channel::Channel()
-{
-}
-
-Channel::~Channel()
-{
-}
 
 #endif
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -3,11 +3,78 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joao-vri <joao-vri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: manelcarvalho <manelcarvalho@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 19:18:51 by joao-vri          #+#    #+#             */
-/*   Updated: 2026/05/13 20:00:53 by joao-vri         ###   ########.fr       */
+/*   Updated: 2026/05/20 18:30:58 by manelcarval      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/Channel.hpp"
+
+Channel::Channel(){}
+
+Channel::Channel(const Channel& other) {
+	*this = other;
+}
+
+Channel::Channel(const std::string channel_name, Client* creator) :
+	_channel_name(channel_name),
+	_invite_active(false),
+	_topic_active(false),
+	_topic(""),
+	_password_active(false),
+	_password(""),
+	_user_limit_active(false),
+	_user_limit(0)
+{
+	_users.insert(creator);
+	_operators.insert(creator);
+	std::cout << "New Channel " << channel_name << " created by " << creator->getNickname() << std::endl;
+}
+Channel& Channel::operator=(const Channel& other) {
+	if (this != &other) {
+		this->_channel_name = other._channel_name;
+		this->_invite_active = other._invite_active;
+		this->_invited = other._invited;
+		this->_operators = other._operators;
+		this->_password = other._password;
+		this->_password_active = other._password_active;
+		this->_topic = other._topic;
+		this->_topic_active = other._topic_active;
+		this->_user_limit = other._user_limit;
+		this->_user_limit_active = other._user_limit_active;
+		this->_users = other._users;
+	}
+	return *this;
+}
+
+Channel::~Channel() {}
+
+void Channel::joinChannel(Client *new_user, std::string password) {
+	if (_invite_active)
+	{
+		if (_invited.count(new_user->getNickname())) 
+		{
+			_users.insert(new_user);
+			std::cout << new_user << " has joined the channel " << this->_channel_name << std::endl;
+			return ;
+		}
+		std::cout << new_user << " wasn't invited to join the channel " << this->_channel_name << std::endl;
+		return ;
+	}
+	if (_password_active && _password != password)
+	{
+		std::cout << "Wrong password, access denied" << std::endl;
+		return ; 
+	}
+	_users.insert(new_user);
+	std::cout << new_user << " has joined the channel " << this->_channel_name << std::endl;
+}
+
+void Channel::partChannel(Client *user_delete) {
+	if (_operators.count(user_delete))
+		_operators.erase(user_delete);
+	_users.erase(user_delete);
+		
+}
