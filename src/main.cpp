506bbaf32a27dc6461c5f4e6b96856cebd4d16b0
jvrs2002 <s.ribeiro.jv@gmail.com>
2026-06-11 16:37:39 +0100

    chore: add basic main boilerplate

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: joao-vri <joao-vri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 19:57:59 by joao-vri          #+#    #+#             */
-/*   Updated: 2026/06/09 20:34:53 by joao-vri         ###   ########.fr       */
+/*   Updated: 2026/06/11 16:36:13 by joao-vri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@
 
 int	main(int argc, char *argv[])
 {
-	if (argc != 3 || !argv)
-		return -1;
-	// call input checking function first
-	Server	server("127.0.0.1", argv[1], argv[2]);
-	while (server.isRunning())
-		// server.run(); still need to create function
+	if (argc != 3 || !isValidInput(argv[1], argv[2])) {
+		std::cerr << "Usage: ./ircserv <port> <password>" << std::endl;
+		return 1;
+	}
+	Server	server("127.0.0.1", std::string(argv[1]), std::string(argv[2]));
+	server.run();
 	return server.getErrorCode();
 }
