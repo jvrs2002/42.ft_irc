cc6049b55a2db036cba4e1183fe4bf351b612093
jvrs2002 <s.ribeiro.jv@gmail.com>
2026-06-19 13:23:20 +0100

    refactor: write the input validation within the main

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: joao-vri <joao-vri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 19:57:59 by joao-vri          #+#    #+#             */
-/*   Updated: 2026/06/12 17:52:29 by joao-vri         ###   ########.fr       */
+/*   Updated: 2026/06/19 13:10:54 by joao-vri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,21 @@
 
 int	main(int argc, char *argv[])
 {
-	if (argc != 3 || !isValidInput(argv[1], argv[2])) { // still need to ask for this function
+	if (argc != 3 || std::strcmp(argv[1], "")) {
 		std::cerr << "Usage: ./ircserv <port> <password>" << std::endl;
 		return 1;
 	}
+
+	std::string	port = argv[1];
+
+	for (unsigned char c : port) {
+		if (!std::isdigit(c)) {
+			std::cerr << "Error: Invalid port '" << port << "'. Port must be a numeric value." << std::endl;
+			return 1;
+		}
+	}
+
+	std::string	password = argv[2];
 	Server	server("127.0.0.1", std::string(argv[1]), std::string(argv[2]));
 	server.run();
 	return server.getErrorCode();
