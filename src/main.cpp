58ce39a4cfec21c14ccdaf9af41129220f9b2577
jvrs2002 <s.ribeiro.jv@gmail.com>
2026-06-19 17:12:04 +0100

    WIP: add signal handler

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: joao-vri <joao-vri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 19:57:59 by joao-vri          #+#    #+#             */
-/*   Updated: 2026/06/19 13:28:57 by joao-vri         ###   ########.fr       */
+/*   Updated: 2026/06/19 17:10:39 by joao-vri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,23 @@
 #include "Commands.hpp"
 #include "Message.hpp"
 #include "Server.hpp"
+#include <csignal>
+
+Server	*g_server_ptr = NULL;
+
+void sigintHandler(int sig) {
+	std::cout << "Interrupt handle " << sig << std::endl; // change msg and maybe broadcast() also
+
+	if (g_server_ptr == NULL) // before run()'s loop
+		exit(sig);
+	else // after run()'s loop
+		g_server_ptr->shutdownServer(EXIT_FAILURE);
+	exit(sig);
+}
 
 int	main(int argc, char *argv[])
 {
-	if (argc != 3 || std::strcmp(argv[1], "")) {
+	if (argc != 3 || argv[1][0] == '\0') {
 		std::cerr << "Usage: ./ircserv <port> <password>" << std::endl;
 		return 1;
 	}
@@ -36,6 +49,7 @@ int	main(int argc, char *argv[])
 
 	std::string	password = argv[2];
 	Server	server("127.0.0.1", std::string(argv[1]), std::string(argv[2]));
+	*g_server_ptr = server;
 	server.run();
 	return server.getErrorCode();
 }
