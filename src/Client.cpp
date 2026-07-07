db51073dbf38a8e6a1a1b33d91522c25ed944505
jvrs2002 <s.ribeiro.jv@gmail.com>
2026-07-07 17:08:23 +0100

    WIP: fix segfault whenever a client joins a channel, leaves and the server gets shutdown

--- src/Client.cpp
+++ src/Client.cpp
@@ -6,7 +6,7 @@
 /*   By: joao-vri <joao-vri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 18:32:06 by joao-vri          #+#    #+#             */
-/*   Updated: 2026/07/07 16:27:18 by joao-vri         ###   ########.fr       */
+/*   Updated: 2026/07/07 17:07:10 by joao-vri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ Client::~Client()
 		close(_socket_fd);
 	
 	_buffer.clear();
-	std::set<Channel*>::iterator it;
+	std::set<Channel*>::iterator it; // error starting from here
 
 	for (it = _channels.begin(); it != _channels.end(); ++it) {
 			(*it)->removeUser(this); // what happens if he's not connected to any?
