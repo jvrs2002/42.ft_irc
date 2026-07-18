f7836c2ea9c78d1594e3f6fbfe0b4ba31853ae4f
jvrs2002 <s.ribeiro.jv@gmail.com>
2026-07-18 18:13:48 +0100

    fix: add client to server should count as an event and nick size limit is 9

--- src/Server.cpp
+++ src/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: joao-vri <joao-vri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 19:18:29 by joao-vri          #+#    #+#             */
-/*   Updated: 2026/07/18 16:11:47 by joao-vri         ###   ########.fr       */
+/*   Updated: 2026/07/18 17:59:02 by joao-vri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -285,16 +285,16 @@ void	Server::processEvents(int events_count)
 						command = active_client->handlePartialBuffer();
 					}
 				}
-				events_count--;
 			}
+			events_count--;
 		}
 		i++;
 	}
 
-/* 	if (events_count != 0) {
+	if (events_count != 0) {
 		std::cerr << "events_count error" << std::endl;
 		shutdownServer(42); // debug possible _pollfd_vector management errors
-	} */
+	}
 }
 
 Client* Server::getClientInstance(int client_fd)
